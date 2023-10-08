84e613d4e73df9906c4a92055233f49931012935
EUIJOON <euijoonism@gmail.com>
2023-10-08 10:42:40 +0900

    스프링으로 전환하기

--- src/main/java/inflearn/spring_core/MemberApp.java
+++ src/main/java/inflearn/spring_core/MemberApp.java
@@ -4,19 +4,24 @@ import inflearn.spring_core.config.AppConfig;
 import inflearn.spring_core.member.Grade;
 import inflearn.spring_core.member.Member;
 import inflearn.spring_core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
 
 //        MemberService memberService = new MemberServiceImpl();//직접 객체생성하던걸
-        AppConfig appConfig = new AppConfig(); // AppConfig를 통해 생성받고 받아오는 걸로 바꿈.
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig(); // AppConfig를 통해 생성받고 받아오는 걸로 바꿈.
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member); // 회원가입이 됨.
 
         Member findMember = memberService.findMember(1L); // 조회
-
         // 찍어보기
         System.out.println("new member = " + member.getName());
         System.out.println("find Member = " + findMember.getName());
--- src/main/java/inflearn/spring_core/config/AppConfig.java
+++ src/main/java/inflearn/spring_core/config/AppConfig.java
@@ -8,27 +8,28 @@ import inflearn.spring_core.member.MemberServiceImpl;
 import inflearn.spring_core.member.MemoryMemberRepository;
 import inflearn.spring_core.order.OrderService;
 import inflearn.spring_core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 애플리케이션의 전체 동작 방식을 구성(config)하기 위해, 구현 객체를 생성하고, 연결하는 책임을 가지는 별도의 설정 클래스
+@Configuration
 public class AppConfig {
-
-    //역할들을 드러나게 바꾼다!
-    // ctrl alt M
-
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
-
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(memberRepository(), discountPolicy());
+        return new OrderServiceImpl(
+                memberRepository(),
+                discountPolicy());
     }
-
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public DiscountPolicy discountPolicy() {
-//        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
 }
