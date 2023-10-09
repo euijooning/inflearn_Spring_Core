23a4671b55cb5a4547fa79dd942bbebbb1732ded
EUIJOON <euijoonism@gmail.com>
2023-10-09 20:14:53 +0900

    탐색 위치와 기본 스캔 대상

--- src/main/java/inflearn/spring_core/AutoAppConfig.java
+++ src/main/java/inflearn/spring_core/AutoAppConfig.java
@@ -7,8 +7,10 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration //설정 정보
 @ComponentScan( // @Component 붙은 자동으로 컴포넌트 정보들을 끌어오는 것
+        basePackages = "inflearn.spring_core.member",
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @Filter(type = FilterType.ANNOTATION, // 그 중에서 뺄 것 @Bean붙은건 수동으로 등록해놓은거니까
-        classes = Configuration.class)
+                                 classes = Configuration.class)
 )
 public class AutoAppConfig {
 
