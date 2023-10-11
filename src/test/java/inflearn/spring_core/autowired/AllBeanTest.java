a5828e920ca97b23bea8d3483889e179ef43ce31
EUIJOON <euijoonism@gmail.com>
2023-10-11 13:27:10 +0900

    조회한 빈이 모두 필요할 때

---
+++ src/test/java/inflearn/spring_core/autowired/AllBeanTest.java
@@ -0,0 +1,60 @@
+package inflearn.spring_core.autowired;
+
+import inflearn.spring_core.AutoAppConfig;
+import inflearn.spring_core.discount.DiscountPolicy;
+import inflearn.spring_core.member.Grade;
+import inflearn.spring_core.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        // 로직 추가
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(discountPrice).isEqualTo(1000);
+
+        int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+        assertThat(rateDiscountPrice).isEqualTo(2000);
+
+    }
+
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        @Autowired
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policies = " + policies);
+        }
+
+        // 여기도 추가
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode); //discountCode를 넣는다.
+
+            System.out.println("discountCode = " + discountCode);
+            System.out.println("discountPolicy = " + discountPolicy);
+
+            return discountPolicy.discount(member, price);
+        }
+
+    }
+}
