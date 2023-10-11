52e9596f0dabb4fe3a80e15dd1e148ab4dd31601
EUIJOON <euijoonism@gmail.com>
2023-10-11 20:15:14 +0900

    인터페이스 InitializingBean, DisposableBean

--- src/test/java/inflearn/spring_core/lifecycle/NetworkClient.java
+++ src/test/java/inflearn/spring_core/lifecycle/NetworkClient.java
@@ -1,31 +1,43 @@
 package inflearn.spring_core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
+public class NetworkClient implements InitializingBean, DisposableBean { // 두 개 추가
     private String url;
 
-    // 생성자
     public NetworkClient() {
-        System.out.println("생성자 호출 url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+        System.out.println("생성자 호출, url = " + url);
     }
 
-    // setter
     public void setUrl(String url) {
         this.url = url;
     }
 
+    //서비스 시작시 호출
     public void connect() {
         System.out.println("connect: " + url);
     }
 
     public void call(String message) {
-        System.out.println("call: " + url + ", message = " + message);
+        System.out.println("call: " + url + " message = " + message);
     }
 
     //서비스 종료시 호출
-    public void disconnect() {
-        System.out.println("close: " + url);
+    public void disConnect() {
+        System.out.println("close + " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disConnect();
     }
 }
